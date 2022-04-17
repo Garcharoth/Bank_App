835c1c33565177f4754b1cdc0aa1f21c2690267b
Garcharoth <gwel.breard@gmail.com>
2022-04-17 11:17:28 +0200

    Méthodes à vide de Current account + add du main

--- Current_Account.py
+++ Current_Account.py
@@ -2,4 +2,11 @@ from Account import Account
 
 
 class Current_Account(Account):
+    """"Class inheriting from the Account class"""
     overdraft: int
+    agios_percentage: int
+
+
+    def apply_agios(self):
+        amount = 0
+        return amount
