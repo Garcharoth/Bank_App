ba33061c27eeb298835a7d547f8884517f033bcc
Garcharoth <gwel.breard@gmail.com>
2022-04-17 16:58:04 +0200

    Début du développement des méthodes de Account, petite correction sur current account

--- Account.py
+++ Account.py
@@ -6,17 +6,24 @@ class Account(ABC):
     """This is an abstract class"""
     account_number: int
     account_holder: string
-    balance: int
+    balance: float
 
+    def __init__(self, account_number: int, account_holder: string, balance: float):
+        self.__account_number: account_number
+        self.__account_holder: account_holder
+        self.__balance = balance
 
-    def withdraw(self):
-        amount = 0
-        return amount
+    def withdraw(self, amount):
+        new_balance = self.balance - amount
+        if new_balance < -100:
+            return print(f"Attention, plafond de découvert atteint, Agios applicables ! Nouveau solde : %i Euros " %new_balance )
+        elif new_balance > -100:
+            return print(f"Nouveau solde : %i Euros" %new_balance)
 
-    def payment(self):
-        amount=0
-        return amount
+    def deposit(self, amount):
+        self.balance += amount
+        return print(f"Le nouveau solde est de : %i Euros" %self.balance)
 
     def show_balance(self):
-        return print(self.balance)
+        return print(f"Votre solde est de : %i Euros" %self.balance)
 
--- Current_Account.py
+++ Current_Account.py
@@ -4,7 +4,7 @@ from Account import Account
 class Current_Account(Account):
     """"Class inheriting from the Account class"""
     overdraft: int
-    agios_percentage: int
+    agios_percentage: float
 
 
     def apply_agios(self):
